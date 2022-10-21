10d0311ecca97368559e3ab55de29001ac1adb69
Mike DelGaudio <delgaudiomike@gmail.com>
2022-10-21 16:43:28 -0400

    add blog page

--- src/components/Blog/blog.page.tsx
+++ src/components/Blog/blog.page.tsx
@@ -3,7 +3,7 @@ import { Header } from "./Header";
 
 const Blog = () => {
   return (
-    <section id="blog" className="relative ">
+    <section id="blog" className="relative pt-4">
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <Header />
         <div className="flex flex-col py-4 gap-10">
